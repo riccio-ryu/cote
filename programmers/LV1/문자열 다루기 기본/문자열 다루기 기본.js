53f9904ce2acd8b3cffc5ce81552544225e4ae58
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2022-08-11 20:09:06 +0900

    Create 문자열 다루기 기본.js

---
+++ programmers/LV1/문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    let answer = true;
+    if(s.length === 4 || s.length === 6){
+        let cas = (Number(s))
+        answer = cas === parseInt(s) ? true : false;
+    }else{
+        answer = false
+    }
+    return answer;
+}
+
+
+
+/*
+function solution(s) {
+  return s.search(/\D/g) < 0 && (s.length === 4 || s.length === 6);
+}
+*/
