c3266898760c460d902d8758ade173a0d5c81962
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2022-08-11 19:27:08 +0900

    Create 문자열 내 마음대로 정렬하기.js

---
+++ programmers/LV1/문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,10 @@
+function solution(strings, n) {
+    return strings.sort((a, b) => {
+        if (a[n] < b[n]) return -1;
+        if (a[n] > b[n]) return 1;
+        if (a[n] === b[n]) return a < b ? -1 : 1;
+        return 0;
+    });
+}
+
+// sort자세히..
