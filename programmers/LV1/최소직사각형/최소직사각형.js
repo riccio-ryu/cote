1246a098bb93dd399b094f0ff06ed7f2cb0b881e
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2022-08-11 13:09:46 +0900

    Update 최소직사각형.js

--- programmers/LV1/최소직사각형/최소직사각형.js
+++ programmers/LV1/최소직사각형/최소직사각형.js
@@ -27,3 +27,9 @@ function solution(sizes) {
   return maxWidth * maxHeight;
 }
 */
+/*
+function solution(sizes) {
+    const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
+    return hor * ver;
+}
+*/
