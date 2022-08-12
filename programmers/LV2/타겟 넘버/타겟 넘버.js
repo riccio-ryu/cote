6bd453655515693d55d0edfc4117a6be48a60f61
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2022-08-12 20:23:42 +0900

    Update 타겟 넘버.js

--- programmers/LV2/타겟 넘버/타겟 넘버.js
+++ programmers/LV2/타겟 넘버/타겟 넘버.js
@@ -17,3 +17,23 @@ function solution(numbers, target) {
     
     return answer;
 }
+
+
+/*
+function solution(numbers, target) {
+    let answer = 0;
+    getAnswer(0,0);
+    function getAnswer(x,value) {
+        if(x<numbers.length){
+            getAnswer(x+1,value + numbers[x]);
+            getAnswer(x+1,value - numbers[x]);
+        } else{
+            if(value === target){
+                answer++
+            }
+        }
+    }
+    return answer;
+}
+
+*/
