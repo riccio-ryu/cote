3bd7ecbde51733b3ed889bd632ae07f92aaf72b5
riccio-ryu <82762183+riccio-ryu@users.noreply.github.com>
2023-05-02 07:45:28 +0900

    재귀

--- bj/코딩 테스트 준비_기초/index.js
+++ bj/코딩 테스트 준비_기초/index.js
@@ -1,31 +1,33 @@
-const input = require('fs').readFileSync('example.txt').toString().trim().split(' ').map(Number);
-// input -> n, m
-//(1 ≤ M ≤ N ≤ 8)
+const input = require('fs').readFileSync('example.txt').toString().trim().split('\n').map(Number);
 
-const N = input[0];
-const M = input[1];
+let arr = [...Array(11)];
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
 
-let ss = (N,M) => {
-    let arr = Array(M).fill(0);     // 자릿수 1 2 3 4 or 1 2
-    // let nums = Array(N).fill(0);    // 숫자의 사용여부? 0 -> 사용안함(사용가능), 1 -> 사용 중
-    let ret = []
-
-    let func = (num) => {
-        // console.log('num  ', num);
-        if(num === M){
-            let data = []
-            for (let i = 0; i < M; i++) {
-                data.push(arr[i]);
-            }
-            ret.push(data.sort((a,b) => a-b))
-        }
-        for (let i = 0; i < N; i++) {
-            arr[num] = i+1
-            // func(num + 1)
-        }
-    }
-
-    func(0)
-    console.log(ret, arr);
+for (let i = 4; i < 11; i++) {
+    arr[i] = arr[i-1] + arr[i-2] + arr[i-3]    
 }
-ss(N,M)
+for (let i = 1; i < input.length; i++) {
+    console.log(arr[input[i]]);
+}
+
+// let arr = [0, 1, 2, 4]
+// for (let i = 1; i <= input.length; i++) {
+//     let num = input[i]
+//     for (let j = 4; j <= num; j++) {
+//         arr[j] = arr[j-1] + arr[j-2] + arr[j-3]
+//     }
+//     console.log(arr[num]);
+// }
+
+/*
+1 (1) -> 1
+2 (2) -> 1,1 / 2
+3 (4) -> 1,1,1 /1,2 /2,1 / 3
+4 (7) -> 1,1,1,1 / 1,1,2 /1,2,1 / 2,1,1 / 2,2 / 1,3 / 3,1
+5 (13)-> 1,1,1,1,1 / 1,1,1,2 / 1,1,2,1 / 1,2,1,1 / 2,1,1,1 / 1,2,2 / 2,1,2 / 2,2,1 / 1,1,3 / 1,3,1/ 3,1,1 / 2,3/ 3,2  
+...
+
+n = n-1 + n-2 + n-3
+*/
