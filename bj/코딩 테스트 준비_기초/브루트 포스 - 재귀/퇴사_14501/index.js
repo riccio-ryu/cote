e003fa3402b484f8294ae674851d9d210e6815e7
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-05-09 16:57:32 +0900

    Create index.js

---
+++ bj/코딩 테스트 준비_기초/브루트 포스 - 재귀/퇴사_14501/index.js
@@ -0,0 +1,58 @@
+//1 ≤ N ≤ 15
+
+const input = require('fs').readFileSync('example.txt').toString().trim().split('\n');
+
+const days = parseInt(input.shift())    // N + 1
+const tp = input.map(x => x.split(' ').map(Number))
+let arr = Array(days).fill(0)
+
+for (let i = 0; i < days; i++) {
+    if(i + tp[i][0] > days) continue;
+    arr[i] += tp[i][1];
+    for (let j = i + tp[i][0]; j < days; j++) {
+        arr[j] = Math.max(arr[j], arr[i])
+    }
+}
+console.log(Math.max(...arr));
+
+
+// let money = 0;
+// let day = 0;
+
+// console.log(days);
+// console.log(tp);
+
+// let ff = (val) => {
+//     let ban = 0;
+//     let arr = Array(days).fill(0)
+
+//     let func = (num, mon) => {
+//         let don = 0;
+//         if(days > num){
+//             console.log('+++ ', num, mon);
+//             console.log('--- ', don);
+//             don += mon;
+//             console.log('*** ', don);
+//         }
+//         for (let i = 0; i < tp.length; i++) {
+//             if(!arr[i]){
+//                 ban += tp[i][1]
+//                 if(days > num + tp[i][0]){
+//                     arr[i] = 1
+//                     func(num + tp[i][0], tp[i][1])
+//                     arr[i] = 0
+//                 }
+//             }
+//         }
+//     }
+//     func(0, 0)
+//     // if(val > days){
+//     //     console.log(val, money, ban, plus);
+//         // money = ban + plus > money ? ban + plus : money
+//     // }else{
+//         // for (let i = 0; i < tp.length; i++) {
+//         //     ff(val + tp[i][0], ban , tp[i][1])
+//         // }
+//     // }
+// }
+// ff(0);
