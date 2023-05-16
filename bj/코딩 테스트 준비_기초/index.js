3a9487461e5b4f6e09736b8a4e4d568600f16633
riccio-ryu <82762183+riccio-ryu@users.noreply.github.com>
2023-05-16 18:57:39 +0900

    순열

--- bj/코딩 테스트 준비_기초/index.js
+++ bj/코딩 테스트 준비_기초/index.js
@@ -1,58 +1,31 @@
-const input = require('fs').readFileSync('example.txt').toString().trim().split('\n');
+// N(1 ≤ N ≤ 10,000)
 
-let cnt, num;
-[cnt, num] = input[0].split(' ').map(Number);
-const str = input[1].split(' ').sort()
-const vow = ['a','e', 'i','o','u']
+const [cnt, ...arr] = require('fs').readFileSync('example.txt').toString().trim().split(/\s+/).map(v => +v);
 
-let ff = (n,m) => {
-    let c = Array(m).fill('')    //자릿수 (cnt)
-    let k = Array(n).fill(0)    // 사용여부(num)
-    let v = []
-    let uu = (t) => {
-        if(t === m){
-            let data = []
-            for (let i = 0; i < m; i++) {
-                data.push(c[i])
-            }
-            v.push(data.sort())
-        }
-        for (let i = 0; i < n; i++) {
-            if(!k[i]){
-                c[t] = str[i]
-                k[i] = 1
-                uu(t+1)
-                k[i]=0
-            }
-        }
+let part = arr[cnt-1]
+let b = 0
+
+for (let i = cnt; i > 0; i--) {
+    if(part <= arr[i - 1]){
+        b = i
+        break;
     }
-    uu(0)
-    const ccc = [...new Set(v.map(o => o.join('')))]
-    for (let i = 0; i < ccc.length; i++) {
-        let z = 0;
-        for (let j = 0; j < vow.length; j++) {
-            if(ccc[i].includes(vow[j])) {
-                z += 1
-            }
-        }
-        if(z > 0){
-            console.log(ccc[i]);
-        }
+    part = arr[i - 1]
+}
+
+let ar1 = arr.splice(0, b-1)
+let ar2 = [...arr]
+
+for (let i = ar2.length - 1; i >= 0; i--) {
+    if(ar1[ar1.length -1] < ar2[i]){
+        let w = ar1[ar1.length -1]
+        ar1.splice(ar1.length -1, 1, ar2[i])
+        ar2.splice(i, 1, w)
+        console.log(ar1.join(' ') + ' ' + ar2.join(' '));
+    }
+    if(i === 0){
+        console.log(-1);
     }
 }
-ff(num, cnt)
 
-// console.log(cnt);
-// console.log(num);
-// console.log(str);
-// let v = ''
-// for (let i = 0; i < cnt; i++) {
-//     let txt = ''
-//     for (let j = i+1; j < num; j++) {
-//         txt = str[i] + str[j]
-//         if(!v.includes(txt)){
-//             v += txt+'\n';
-//         }
-//     }
-// }
-// console.log(v);
+// 참조 : https://kwanghyuk.tistory.com/25
