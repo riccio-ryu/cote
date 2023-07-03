a98f8fbfe41c6bf3dfaa57eb6cf64d4ccb124038
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-07-03 21:30:45 +0900

    Create 공원 산책.js

---
+++ programmers/LV1/공원 산책/공원 산책.js
@@ -0,0 +1,53 @@
+/* 정확성 75.0
+function solution(park, routes) {
+    let start = [0,0]       // 시작위치(현재 위치)
+    park.map((x, i) => {    // 시작점 찾아 놓기
+        let s = x.indexOf("S")
+        if(s !== -1){
+            return start = [i,s]
+        }
+    })
+    routes.map(x => {
+        let path = x.split(' ');
+        let d = path[0];
+        let n = +path[1];
+        switch(d){
+            case "N":
+                let vertN = v(park, start[1])
+                let strN = vertN.slice(start[0]+1-n, start[0]+1)
+                if(vertN.slice(0, start[0]+1).length < n || fd(strN)) break;
+                start = [start[0] - n, start[1]]
+                break;
+            case "E":
+                let strE = park[start[0]].slice(start[1]+1, start[1]+1+n)   // S search next part
+                if(park[start[0]].slice(start[1]+1).length < n || fd(strE)) break;
+                start = [start[0], start[1] + n]
+                break;
+            case "W":
+                let strW = park[start[0]].slice(start[1]+1-n, start[1]+1)
+                if(park[start[0]].slice(0, start[1]+1).length < n || fd(strW)) break;
+                start = [start[0], start[1] - n]
+                break;
+            case "S":
+                let vertS = v(park, start[1])
+                let strS = vertS.slice(start[0]+1, start[0]+1+n)
+                if(vertS.slice(start[0]+1).length < n || fd(strS)) break;
+                start = [start[0] + n, start[1]]
+                break;
+            default:
+                console.log('default')
+        }
+    })
+    return start;
+}
+const fd = (t) => {
+    return t.includes('X')
+}
+const v = (park, num) => {
+    let txt = "";
+    park.map(k => {
+        txt += k.slice(num, num+1)
+    })
+    return txt;
+}
+*/
