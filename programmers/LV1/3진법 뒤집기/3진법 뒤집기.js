a67a629a60945af134e0688f2376f3979aec8738
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-08-29 22:40:30 +0900

    Update 3진법 뒤집기.js

--- programmers/LV1/3진법 뒤집기/3진법 뒤집기.js
+++ programmers/LV1/3진법 뒤집기/3진법 뒤집기.js
@@ -1,3 +1,11 @@
+//2023
+function solution(n) {
+    let t = n.toString(3)
+    let r = t.split('').reverse().join('')
+    return parseInt(r, 3);
+}
+
+//2022
 function solution(n) {
     let answer = '';
     let thr = n.toString(3).split('')
