950844a5aa12e24bf5d3aa82bd3d18f69610f3d8
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-08-30 00:08:03 +0900

    Update 크레인 인형뽑기 게임.js

--- programmers/LV1/크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ programmers/LV1/크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,3 +1,26 @@
+//2023
+function solution(board, moves) {
+    const arr = [];
+    let answer = 0;
+    moves.forEach(m => {
+        for(let i = 0; i < board.length; i++){
+            const c = board[i][m-1];
+            if(c !== 0){
+                if(c === arr.slice(-1)[0]){
+                    arr.pop()
+                    answer += 2;
+                }else{
+                    arr.push(c)
+                }
+                board[i][m-1] = 0
+                break;
+            }
+        }
+    })
+    return answer;
+}
+
+//2022
 function solution(board, moves) {//b: [[],[]], m:[n,n,n], retern: dis num
     let basket =[];
     let boom = 0;
