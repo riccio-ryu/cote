b5e5d5ff288d1cb4687a5882f28716dc4e9b6a9e
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-08-29 22:35:29 +0900

    Update 내적.js

--- programmers/LV1/내적/내적.js
+++ programmers/LV1/내적/내적.js
@@ -1,3 +1,13 @@
+//2023
+function solution(a, b) {
+    let answer = 0;
+    for(let i = 0; i < a.length; i++){
+        answer += a[i] * b[i]
+    }
+    return answer;
+}
+
+//2022
 function solution(a, b) {
     return a.reduce((x,y,i) => x+(y*b[i]), 0)
 }
