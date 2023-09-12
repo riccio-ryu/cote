155cb5d97389940de6b9b63f24bd9cd9411d86cc
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-12 21:19:21 +0900

    Update 하샤드 수.js

--- programmers/LV1/하샤드 수/하샤드 수.js
+++ programmers/LV1/하샤드 수/하샤드 수.js
@@ -1,3 +1,9 @@
+//2023
+function solution(x) {
+    return x % String(x).split('').reduce((a,b) => +a + +b,0) ? false : true;
+}
+
+//2022
 function solution(x) {
     const h = x.toString().split('').map(x=>parseInt(x)).reduce((a,c)=>a+c,0)
     return x%h === 0 ? true : false;
