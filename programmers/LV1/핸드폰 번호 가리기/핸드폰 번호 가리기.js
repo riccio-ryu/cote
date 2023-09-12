c9946ec26abffa0e12248e90a5f6ffcfe4bfb837
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-12 21:19:00 +0900

    Update 핸드폰 번호 가리기.js

--- programmers/LV1/핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ programmers/LV1/핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,8 @@
+//2023
+function solution(phone_number) {
+    return phone_number.slice(-4).padStart(phone_number.length, '*');
+}
+//2022
 function solution(phone_number) {
     return "*".repeat(phone_number.length-4)+phone_number.slice(-4);
 }
