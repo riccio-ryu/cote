67f2cc653eafb3875770ea63df11728722cede00
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-13 02:51:20 +0900

    Update 폰켓몬.js

--- programmers/LV1/폰켓몬/폰켓몬.js
+++ programmers/LV1/폰켓몬/폰켓몬.js
@@ -1,3 +1,12 @@
+//2023
+function solution(nums) {
+    const s = Array.from(new Set(...[nums]))
+    const c = nums.length/2
+    console.log(s,c)
+    return s.length > c ? c : s.length;
+}
+
+//2022
 function solution(nums) {
     let arr = Array.from(new Set(nums))
     const hf = nums.length/2
