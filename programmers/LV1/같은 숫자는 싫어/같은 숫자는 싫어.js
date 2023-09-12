c1c23b7d8a5f15428a4f4becdf77ba9204c36f49
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-13 02:31:52 +0900

    Update 같은 숫자는 싫어.js

--- programmers/LV1/같은 숫자는 싫어/같은 숫자는 싫어.js
+++ programmers/LV1/같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,15 @@
+//2023
+function solution(arr)
+{
+    let answer = [];
+    for(const x of arr){
+        if(x === answer[answer.length-1]) continue;
+        answer.push(x)
+    }
+    return answer;
+}
+
+//2022
 function solution(arr)
 {
     let answer = [];
