0c1d1ff89e2d72d5440748b8b7bbc6fb3731b55d
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-13 01:50:54 +0900

    Update 문자열 다루기 기본.js

--- programmers/LV1/문자열 다루기 기본/문자열 다루기 기본.js
+++ programmers/LV1/문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,3 +1,9 @@
+//2023
+function solution(s) {
+    return parseInt(s) === +s && (s.length === 4 || s.length === 6) ? true : false;
+}
+
+//2022
 function solution(s) {
     let answer = true;
     if(s.length === 4 || s.length === 6){
