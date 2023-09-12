8a1aeb17f200c6a1b276a12bdd56789451b61bf8
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-13 01:18:17 +0900

    Update 서울에서 김서방 찾기.js

--- programmers/LV1/서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ programmers/LV1/서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,3 +1,9 @@
+//2023
+function solution(seoul) {
+    return `김서방은 ${seoul.indexOf('Kim')}에 있다`;
+}
+
+//2022
 function solution(seoul) {
     let answer = 0;
     for(let i =0; i < seoul.length; i++){
