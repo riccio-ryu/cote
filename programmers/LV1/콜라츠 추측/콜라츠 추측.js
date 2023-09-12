3e9f3ed95d72e5801fcb801812821b5afdddcfe4
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-12 21:20:03 +0900

    Update 콜라츠 추측.js

--- programmers/LV1/콜라츠 추측/콜라츠 추측.js
+++ programmers/LV1/콜라츠 추측/콜라츠 추측.js
@@ -1,3 +1,27 @@
+//2023
+function solution(num) {
+    let answer = 0;
+    if(num < 2){
+        return 0
+    }
+    const fc = (n) => {
+        if(answer >= 500){
+            return answer = -1
+        }else if(n===1){
+            return answer
+        }else if(n%2 ===0){
+            answer++
+            fc(n/2)
+        }else{
+            answer++
+            fc(n*3 + 1)
+        }
+    }
+    fc(num)
+    return answer;
+}
+
+//2022
 function solution(num) {
     let answer = 0;
     let cast = (num) => {
