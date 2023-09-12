d5f3df46575951f1796c4041d9102472dd1bb9a5
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-12 21:38:52 +0900

    Update 이상한 문자 만들기.js

--- programmers/LV1/이상한 문자 만들기/이상한 문자 만들기.js
+++ programmers/LV1/이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,3 +1,20 @@
+//2023
+function solution(s) {
+    const arr = s.split(' ')
+    return arr.map(a => {
+        const b = a.split('')
+        for(let i=0; i<b.length; i++){
+            if(i%2){
+                b[i] = b[i].toLowerCase()
+            }else{
+                b[i] = b[i].toUpperCase()
+            }
+        }
+        return b.join('')
+    }).join(' ');
+}
+
+//2022
 function solution(s) {
     const arr = s.split(" ")
     return arr.map((x) => {
