816bbe54c38eca5402184d8b80e11667886cb1b5
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-12 21:20:34 +0900

    Update 제일 작은 수 제거하기.js

--- programmers/LV1/제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ programmers/LV1/제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,3 +1,23 @@
+//2023
+function solution(arr) {
+    // if(arr.length < 2){
+    //     return [-1]
+    // }else{
+    //     arr.sort((a,b) => b-a).pop()
+    // }
+    // return arr;
+    
+    // 아.. 배열의 순서 안바뀌어야 함
+    
+    if(arr.length < 2){
+        return [-1]
+    }else{
+        arr.splice(arr.indexOf(Math.min(...arr)), 1)
+    }
+    return arr
+}
+
+//2022
 function solution(arr) {
     let answer = [];
     const mn = Math.min(...arr)
