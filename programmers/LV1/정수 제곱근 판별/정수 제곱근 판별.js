6997b925e11b23aa71d9746bcf171980a7fa0311
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-09-12 21:24:20 +0900

    Update 정수 제곱근 판별.js

--- programmers/LV1/정수 제곱근 판별/정수 제곱근 판별.js
+++ programmers/LV1/정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,3 +1,9 @@
+//2023
+function solution(n) {
+    return Math.sqrt(n)%1 ? -1: Math.pow(Math.sqrt(n)+1, 2);
+}
+
+//2022
 function solution(n) {
     const sqrt = Math.sqrt(n)
     return sqrt%1 === 0 ? Math.pow(sqrt+1, 2) : -1;
