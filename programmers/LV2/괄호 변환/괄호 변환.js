6b9f9b731915f45b4638842ecd968db103369012
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-12-12 15:11:33 +0900

    Update 괄호 변환.js

--- programmers/LV2/괄호 변환/괄호 변환.js
+++ programmers/LV2/괄호 변환/괄호 변환.js
@@ -1,3 +1,31 @@
+// 2023
+function solution(s) {
+    let cs = s.split('')
+    let answer = 0;
+    for(let i = 0; i< cs.length; i++){
+        const copy = [...cs]
+        const stack = []
+        // console.log(copy)
+        while(copy.length){
+            const sh = copy.shift()
+            // console.log(sh)
+            if(!stack.length) stack.push(sh)
+            else{
+                const pp = stack.pop()
+                if(!(pp+sh === '()' || pp+sh === '[]' || pp+sh === '{}')) {
+                    stack.push(pp)
+                    stack.push(sh)
+                }
+            }
+        }
+        if(!stack.length) answer++
+        // console.log(copy)
+        cs.push(cs.shift()) // 마지막에 섞어주자
+    }
+    return answer;
+}
+
+//2022
 function solution(p) {
     if(p.length === 0) return p
     let answer = '';
