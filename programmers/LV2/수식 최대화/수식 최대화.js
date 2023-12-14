5c3f980200cbda3d3273a9697cd66323b20e95fc
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-12-14 21:53:38 +0900

    Update 수식 최대화.js

--- programmers/LV2/수식 최대화/수식 최대화.js
+++ programmers/LV2/수식 최대화/수식 최대화.js
@@ -1,3 +1,45 @@
+// 2023
+
+function solution(expression) {
+    let answer = 0;
+    const symbol = ['*', '+', '-']
+    const symbolArr = [['*', '+', '-'],['*', '-', '+'],['+', '*', '-'],['+', '-', '*'],['-', '*', '+'],['-', '+', '*']]
+    for(const s of symbolArr){  // s symbolArr의 배열 한 부분
+        const es = expression.split(/(\D)/)
+        // console.log(s, es)
+        for(const ss of s){     // symbolArr의 배열 한 부분의 한 원소
+            // console.log(es,ss, es.includes(ss))
+            while(es.includes(ss)){
+                const i = es.indexOf(ss)
+                // console.log(i)
+                es.splice(i-1,3, cal(es.slice(i-1, i+2)[0], es.slice(i-1, i+2)[2], es.slice(i-1, i+2)[1]))
+                // es.splice()
+                // console.log('es slice :', es.slice(i-1, i+2), cal(es.slice(i-1, i+2)[0], es.slice(i-1, i+2)[2], es.slice(i-1, i+2)[1]))
+                // console.log('splcie', es.splice(i-1,3, cal(es.slice(i-1, i+2)[0], es.slice(i-1, i+2)[2], es.slice(i-1, i+2)[1])))
+                // console.log('bf spl', es)
+            }
+        }
+        // console.log('f', es)
+        answer = answer > Math.abs(es) ? answer : Math.abs(es)
+    }
+    return answer;
+}
+
+const cal = (a,b,o) => {
+    // console.log('abo : ', a,b,o)
+    a = +a
+    b = +b
+    if(o === '*') return a*b
+    if(o === '+') return a+b
+    if(o === '-') return a-b
+}
+
+
+
+
+
+//2022
+
 function solution(expression) {
     var answer = 0;
     const mathExp = ['*', '+', '-']
