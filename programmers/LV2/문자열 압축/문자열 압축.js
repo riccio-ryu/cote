984e8b41bd7bbe281dd78dd44d2c7d765b87a569
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-12-21 22:53:15 +0900

    Update 문자열 압축.js

--- programmers/LV2/문자열 압축/문자열 압축.js
+++ programmers/LV2/문자열 압축/문자열 압축.js
@@ -1,3 +1,35 @@
+//2023
+
+function solution(s) {
+    const l = s.length
+    let answer = l;
+    for(let i = 1; i<= Math.floor(l / 2); i++){//i는 자를 수(절반보다 많은 수를 자를수 없음) 
+        let temp = "";  // 문자열 임시저장
+        let cnt = 1;    // 같은 문자열 개수
+        for (let j = 0; j < l; j += i) {        // j는 i만큼 늘어난다
+            let ss = s.slice(j, j + i);         // 비교대상 문자열
+            // console.log('i', i, 'j', j, 'ss', ss)
+            while(true){                        // 같은 문자열을 찾는다
+                j+=i    // 현재 다음
+                let ss2 = s.slice(j, j + i);
+                console.log(ss, ss2)
+                if(ss === ss2) cnt++
+                else break;
+            }
+            temp += cnt >= 2 ? cnt + ss : ss;
+            j-=i        // 현재 다음 초기화
+            cnt = 1     //cnt초기화
+        }
+        // console.log('temp : ', temp)
+        answer = Math.min(temp.length, answer);
+        // console.log(answer)
+    }
+    return answer;
+}
+
+
+
+//2022
 function solution(s) {
     let answer = 0;
     const leng = s.length;
