2802a9579ec88a57c91ebc35a699f04163c1696b
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2023-12-26 20:41:05 +0900

    Update 타겟 넘버.js

--- programmers/LV2/타겟 넘버/타겟 넘버.js
+++ programmers/LV2/타겟 넘버/타겟 넘버.js
@@ -1,3 +1,23 @@
+// 2023
+//dfs
+function solution(numbers, target) {
+    let answer = 0;
+    const dfs = (sum, idx) => {
+        if(idx === numbers.length){
+            if(sum === target) answer++
+            return
+        }
+        dfs(sum + numbers[idx], idx+1)
+        dfs(sum - numbers[idx], idx+1)
+    }
+    dfs(0,0)
+    return answer;
+}
+
+
+
+
+// 2022
 function solution(numbers, target) {
     let answer = 0;
     
