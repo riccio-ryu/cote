2d33abcc702b13f27bb5530452d9fd35203f644a
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-01-05 17:55:48 +0900

    Create 가장 큰 수.js

---
+++ programmers/LV2/가장 큰 수/가장 큰 수.js
@@ -0,0 +1,7 @@
+// 2023
+
+// 처음에 11번 실패가 났는데, 찾아보니, 0인 경우도 있다했다 numbers = [0]만 되어 있는 경우는 가장 큰 것이 0으로 해야한다.
+function solution(numbers) {
+    let answer = numbers.map(n => String(n)).sort((a,b) => (b+a) - (a+b)).join('')
+    return answer[0] === '0' ? '0' : answer;;
+}
