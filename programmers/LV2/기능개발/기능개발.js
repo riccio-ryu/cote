b89b74d308a518127b4e19516144cfae3e95e39a
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-01-14 15:26:31 +0900

    Update 기능개발.js

--- programmers/LV2/기능개발/기능개발.js
+++ programmers/LV2/기능개발/기능개발.js
@@ -1,3 +1,25 @@
+//2024
+function solution(progresses, speeds) {
+    const answer = [];
+    const arr = progresses.map((p,i,a) => {
+        return Math.ceil((100 - p) / speeds[i])
+    })
+    // console.log(arr)
+    let max = arr[0]    // 현재 가장 큰수
+    answer.push(1)      // 0번째 인덱스부터 출발
+    for(let i = 1; i < arr.length; i++){
+        // console.log(i, arr[i], max)
+        if(arr[i] <= max){
+            answer[answer.length - 1]++
+        }else{
+            answer.push(1)
+            max = arr[i]
+        }
+    }
+    return answer;
+}
+
+//2022
 function solution(progresses, speeds) {
     let answer = [];
     const tas = progresses.map((prog, index) => {
