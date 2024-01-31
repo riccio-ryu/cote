bac6617f30d44d58562cb010836819f5b6b5ac41
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-01-31 18:31:37 +0900

    Update 짝지어 제거하기.js

--- programmers/LV2/짝지어 제거하기/짝지어 제거하기.js
+++ programmers/LV2/짝지어 제거하기/짝지어 제거하기.js
@@ -1,3 +1,18 @@
+// 2024
+function solution(s)
+{
+    let answer = 0;
+    const arr = []
+    for(let i = 0; i< s.length; i++){
+        // console.log(s[i], arr.at(-1))
+        arr.at(-1) === s[i] ? arr.pop() : arr.push(s[i])
+    }
+    // console.log(arr)
+    return arr.length ? 0 : 1;
+}
+
+//2022
+
 function solution(s)
 {
     let answer = -1;
