bc26e6b29421ebb2131f1552348c801f2fe704d0
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-02-21 19:31:43 +0900

    Create 다음 큰 숫자.js

---
+++ programmers/LV2/땅따먹기/다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,11 @@
+function solution(n) {;
+    let n2 = n.toString(2).split('').filter(f => f === '1').length
+    let num = n+1
+    while(true){
+        const n3 = num.toString(2).split('').filter(f => f === '1').length
+        if(n2 === n3){
+            return num
+        }
+        num++
+    }
+}
