69f541e648228b3ebf396452150ab2d189f1af77
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-02-21 19:36:45 +0900

    Update 올바른 괄호.js

--- programmers/LV2/올바른 괄호/올바른 괄호.js
+++ programmers/LV2/올바른 괄호/올바른 괄호.js
@@ -1,3 +1,14 @@
+//2024
+function solution(s){
+    let answer = 0;
+    for(const x of s){
+        if(x === '(') answer++
+        if(x === ')') answer--
+        if(answer < 0) return false
+    }
+    return answer === 0 ? true : false;
+}
+//2022
 function solution(s){
     let cnt = 0;
     
