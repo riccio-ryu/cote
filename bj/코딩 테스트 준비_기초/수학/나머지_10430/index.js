7fe509b780ba44b6a24d028df8698d175d037d7b
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-05-14 20:06:46 +0900

    Update index.js

--- bj/코딩 테스트 준비_기초/수학/나머지_10430/index.js
+++ bj/코딩 테스트 준비_기초/수학/나머지_10430/index.js
@@ -1,10 +1,25 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+// 2024
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map((a) => parseInt(a));
 
-const A = parseInt(input[0]);
-const B = parseInt(input[1]);
-const C = parseInt(input[2]);
+const [A, B, C] = input;
 
-console.log((A+B)%C);
-console.log(((A%C)+(B%C))%C);
-console.log((A*B)%C);
-console.log(((A%C) * (B%C))%C);
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
+
+//2023
+// const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+// const A = parseInt(input[0]);
+// const B = parseInt(input[1]);
+// const C = parseInt(input[2]);
+
+// console.log((A+B)%C);
+// console.log(((A%C)+(B%C))%C);
+// console.log((A*B)%C);
+// console.log(((A%C) * (B%C))%C);
