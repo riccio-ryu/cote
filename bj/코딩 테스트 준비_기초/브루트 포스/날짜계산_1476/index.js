8099f1be9d1e6e3869eca5263f1ec8925abbf1ca
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-05-22 02:01:35 +0900

    Update index.js

--- bj/코딩 테스트 준비_기초/브루트 포스/날짜계산_1476/index.js
+++ bj/코딩 테스트 준비_기초/브루트 포스/날짜계산_1476/index.js
@@ -1,3 +1,24 @@
+// 2024
+const input = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const [e, s, m] = input;
+let x = 1;
+while (true) {
+  if ((x - e) % 15 === 0 && (x - s) % 28 === 0 && (x - m) % 19 === 0) {
+    console.log(x);
+    break;
+  }
+  x++;
+}
+// 메모리 초과... node론 풀수가 없단다...
+
+
+// 2023
 const input = require('fs').readFileSync('example.txt').toString().trim().split(' ').map(Number);
 
 let esm = (e,s,m) => {
