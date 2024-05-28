b90b0d9e2c13c8ebcf7b337a2dd0302299c0c132
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-05-28 17:22:55 +0900

    Create index.js

---
+++ bj/코딩 테스트 준비_기초/브루트 포스/리모컨_1107/index.js
@@ -0,0 +1,44 @@
+const input = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((s) => s.replace(/\n|\r|\s*/g, ""));
+
+// console.log(input);
+const N = +input[0];
+const M = +input[1];
+const nums = input[2].split("").map(Number).sort();
+
+const brokens = nums
+  ? nums.reduce((acc, v) => {
+      acc[v] = true;
+      return acc;
+    }, {})
+  : {};
+
+console.log(N, M, nums, brokens);
+
+let count = Math.abs(1000000 - N);
+for (let i = 0; i < 100; i++) {
+  //1000000 -> +500000 , -500000
+  const n = i.toString();
+  let isValid = true; // cnt세기 위해
+  // console.log(n, isValid);
+  for (let j = 0; j < n.length; j++) {
+    // console.log(n, j, brokens, brokens[n[j]]);
+    if (brokens[n[j]]) {
+      // 속해 있는지
+      // console.log(n, j, brokens, brokens[n[j]]);
+      isValid = false;
+      break;
+    }
+  }
+  // console.log(isValid, count, Math.abs(i - N) + n.length);
+  if (isValid) {
+    //brokens에 속해 있지 않은수의 count를 센다
+    count = Math.min(count, Math.abs(i - N) + n.length);
+    // console.log(count);
+  }
+}
+console.log(count);
