114e38d1ba0d5287545a6d5c60c2687a8f9fc3a8
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-06-16 16:23:33 +0900

    Create index.js

---
+++ bj/코딩 테스트 준비_기초/브루트 포스 - N과 M/N과 M (6)_15655/index.js
@@ -0,0 +1,47 @@
+// 2024
+
+const [[N, M], O] = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .split("\n")
+  .map((s) => s.replace(/\r/g, "").split(" ").map(Number));
+
+const num = O.sort((a, b) => a - b);
+const arr = Array(M).fill("");
+const visited = Array(N + 1).fill(false);
+// console.log(N, M, num);
+let b = [];
+
+// console.log(arr, visited);
+
+function dfs(lev) {
+  if (lev === M) {
+    // lev이 M과 같을때 return한다.
+    let data = [];
+    for (let i = 0; i < M; i++) {
+      // console.log("arr i ", arr[i]);
+      data.push(arr[i]);
+    }
+    // console.log(data);
+    return b.push(data.sort((a, b) => a - b));
+    // return (b += `${data.join(" ")}\n`);
+  }
+  for (let i = 1; i <= N; i++) {
+    //     console.log("i = " + i);
+    if (visited[i]) {
+      continue;
+    }
+    arr[lev] = num[i - 1];
+    visited[i] = true;
+    dfs(lev + 1);
+    visited[i] = false;
+  }
+}
+dfs(0);
+const bb = [...new Set(b.join("|").split("|"))].map((a) =>
+  a.split(",").join(" ")
+);
+// console.log(bb);
+for (let i = 0; i < bb.length; i++) {
+  console.log(bb[i]);
+}
