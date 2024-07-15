2b2a42a82730b16966070349c64cbb9cc42c49b9
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-07-16 00:17:28 +0900

    Update index.js

--- bj/코딩 테스트 준비_기초/브루트 포스 - 순열/이전 순열_10973/index.js
+++ bj/코딩 테스트 준비_기초/브루트 포스 - 순열/이전 순열_10973/index.js
@@ -1,3 +1,53 @@
+// 2024
+const fs = require("fs");
+
+// 입력을 읽어옵니다.
+const input = fs.readFileSync("example.txt").toString().trim().split("\n");
+const n = parseInt(input[0]);
+let m = input[1].split(" ").map(Number);
+
+function ff(arr) {
+  let i = arr.length - 1;
+
+  while (i > 0 && arr[i - 1] <= arr[i]) {
+    i--;
+  }
+
+  if (i <= 0) {
+    return false;
+  }
+
+  let j = arr.length - 1;
+
+  while (arr[j] >= arr[i - 1]) {
+    j--;
+  }
+  // console.log(1, arr, i, j);
+  [arr[i - 1], arr[j]] = [arr[j], arr[i - 1]];
+  // console.log(2, arr);
+  // arr[i]부터 끝까지 배열을 뒤집습니다.
+  j = arr.length - 1;
+  while (i < j) {
+    // console.log(3, arr);
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    // console.log(4, arr);
+    i++;
+    j--;
+  }
+
+  return true;
+}
+
+if (ff(m)) {
+  console.log(m.join(" "));
+} else {
+  console.log(-1);
+}
+
+
+
+// 2023
+
 // N(1 ≤ N ≤ 10,000)
 
 const [cnt, ...arr] = require('fs').readFileSync('example.txt').toString().trim().split(/\s+/).map(v => +v);
