beedce8dd14c2b29e68c6ead929109c87d9f6d1b
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-10-20 21:42:38 +0900

    Update index.js

--- bj/코딩 테스트 준비_기초/다이나믹 프로그래밍 Part 2/타일 채우기_2133/index.js
+++ bj/코딩 테스트 준비_기초/다이나믹 프로그래밍 Part 2/타일 채우기_2133/index.js
@@ -1,18 +1,20 @@
 // 2024
 
-let n = require("fs").readFileSync("example.txt").toString().trim();
-let dp = Array(n + 1).fill(0);
+const fs = require("fs");
+const input = parseInt(fs.readFileSync("/dev/stdin").toString().trim(), 10);
+
+let dp = Array(input + 1).fill(0);
 dp[0] = 1;
 
-if (n >= 2) {
+if (input >= 2) {
   dp[2] = 3;
 }
 
-for (let i = 4; i <= n; i += 2) {
+for (let i = 4; i <= input; i += 2) {
   dp[i] = dp[i - 2] * 3;
   for (let j = i - 4; j >= 0; j -= 2) {
     dp[i] += dp[j] * 2;
   }
 }
 
-console.log(dp[n]);
+console.log(dp[input]);
