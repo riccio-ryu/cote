4685746912cfa5a1b5db6277cfbafb9d33362dd6
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2024-10-23 22:49:45 +0900

    Create index.js

---
+++ bj/코딩 테스트 준비_기초/큐와 그래프/덱_10866/index.js
@@ -0,0 +1,36 @@
+// 2024
+
+let [n, ...list] = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((s) => s.replace(/\n|\r*/g, ""));
+n = Number(n);
+
+const q = [];
+let result = "";
+
+for (let i = 0; i < n; i++) {
+  const command = list[i].split(" ");
+  // console.log(i, list[i], command);
+  if (command[0] === "push_front") {
+    q.unshift(parseInt(command[1], 10));
+  } else if (command[0] === "push_back") {
+    q.push(parseInt(command[1], 10));
+  } else if (command[0] === "pop_front") {
+    result += (q.length ? q.shift() : -1) + "\n";
+  } else if (command[0] === "pop_back") {
+    result += (q.length ? q.pop() : -1) + "\n";
+  } else if (command[0] === "size") {
+    result += q.length + "\n";
+  } else if (command[0] === "empty") {
+    result += (q.length ? 0 : 1) + "\n";
+  } else if (command[0] === "front") {
+    result += (q.length ? q[0] : -1) + "\n";
+  } else if (command[0] === "back") {
+    result += (q.length ? q[q.length - 1] : -1) + "\n";
+  }
+}
+
+console.log(result);
