08e148f1d50a56e8d17abca56aaca6d09dbeb9dd
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2025-01-05 21:39:33 +0900

    Create index.js

---
+++ bj/코딩 테스트 준비_연습/브루트 포스 - 재귀/연산자 끼워넣기_14888/index.js
@@ -0,0 +1,49 @@
+// 2024
+
+
+const input = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((s) => s.replace(/\n|\r/g, "").split(" ").map(Number));
+const [N] = input[0];
+const arr = input[1];
+const cal = input[2];
+// console.log(arr, cal, N);
+let max = -Infinity;
+let min = Infinity;
+
+const calculate = (a, b, o) => {
+  switch (o) {
+    case 0:
+      return a + b;
+    case 1:
+      return a - b;
+    case 2:
+      return a * b;
+    case 3:
+      return a < 0 ? -Math.floor(-a / b) : Math.floor(a / b);
+  }
+};
+
+const act = (i, res) => {
+  if (i === N) {
+    max = Math.max(max, res);
+    min = Math.min(min, res);
+    return;
+  }
+
+  for (let j = 0; j < 4; j++) {
+    if (cal[j] > 0) {
+      cal[j]--;
+      act(i + 1, calculate(res, arr[i], j));
+      cal[j]++;
+    }
+  }
+};
+
+act(1, arr[0]);
+
+console.log(max);
+console.log(min);
