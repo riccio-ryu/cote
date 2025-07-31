187ea4313e6d9d9b87cb44fb8bdbc806c96a0c46
Yeon-Chang <82762183+riccio-ryu@users.noreply.github.com>
2025-07-31 15:54:29 +0900

    Update index.js

--- bj/코딩 테스트 준비 - 문제/브루트 포스 Part 1/숫자 재배치_16943/index.js
+++ bj/코딩 테스트 준비 - 문제/브루트 포스 Part 1/숫자 재배치_16943/index.js
@@ -18,9 +18,7 @@ function dfs(path, used) {
         return;
     }
     for (let i = 0; i < a.length; i++) {
-        // console.log(`Checking: ${a[i]}, Used: ${used[i]}, path: ${path}, used: ${used}`);
         if (used[i]) continue;
-        // 중복 숫자 방지
         if (i > 0 && a[i] === a[i-1] && !used[i-1]) continue;
         used[i] = true;
         path.push(a[i]);
@@ -30,13 +28,14 @@ function dfs(path, used) {
     }
 }
 
-a.sort((x, y) => y.localeCompare(x)); // 내림차순 정렬(최대값부터 탐색)
-if (a.length < b.length) {
-    // 그냥 내림차순으로 붙이면 됨
+a.sort((x, y) => y.localeCompare(x)); // 내림차순 정렬
+
+if (a.length > b.length) {
+    answer = -1;
+} else if (a.length < b.length) {
     if (a[0] !== '0') {
         answer = +a.join('');
     } else {
-        // 0이 맨 앞에 오면 안 됨
         for (let i = 1; i < a.length; i++) {
             if (a[i] !== '0') {
                 [a[0], a[i]] = [a[i], a[0]];
@@ -46,8 +45,60 @@ if (a.length < b.length) {
         }
     }
 } else {
-    // console.log(a);
     dfs([], Array(a.length).fill(false));
 }
 
 console.log(answer);
+
+
+// const input = require("fs").readFileSync("example.txt").toString().split(" ").map(Number);
+// let [a, b] = input;
+
+// a = a.toString().split('');
+// b = b.toString();
+
+// let answer = -1;
+
+// function dfs(path, used) {
+//     if (path.length === a.length) {
+//         if (path[0] === '0') return;
+//         const num = path.join('');
+//         if (num < b && +num > answer) {
+//             answer = +num;
+//         }
+//         return;
+//     }
+//     for (let i = 0; i < a.length; i++) {
+//         // console.log(`Checking: ${a[i]}, Used: ${used[i]}, path: ${path}, used: ${used}`);
+//         if (used[i]) continue;
+//         // 중복 숫자 방지
+//         if (i > 0 && a[i] === a[i-1] && !used[i-1]) continue;
+//         used[i] = true;
+//         path.push(a[i]);
+//         dfs(path, used);
+//         path.pop();
+//         used[i] = false;
+//     }
+// }
+
+// a.sort((x, y) => y.localeCompare(x)); // 내림차순 정렬(최대값부터 탐색)
+// if (a.length < b.length) {
+//     // 그냥 내림차순으로 붙이면 됨
+//     if (a[0] !== '0') {
+//         answer = +a.join('');
+//     } else {
+//         // 0이 맨 앞에 오면 안 됨
+//         for (let i = 1; i < a.length; i++) {
+//             if (a[i] !== '0') {
+//                 [a[0], a[i]] = [a[i], a[0]];
+//                 answer = +a.join('');
+//                 break;
+//             }
+//         }
+//     }
+// } else {
+//     // console.log(a);
+//     dfs([], Array(a.length).fill(false));
+// }
+
+// console.log(answer);
